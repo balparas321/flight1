f836dae538a234a452a80700c590c6d54039aa0f
balparas321 <parasramola97299@gmail.com>
2022-02-02 00:36:47 +0530

    updated Regularticket class

--- Regular_Ticket.java
+++ Regular_Ticket.java
@@ -2,66 +2,26 @@ package Reservation_System;
 
 import java.io.PrintStream;
 
-public class Regular_Ticket extends Ticket{
 
+public class Regular_Ticket extends Ticket {
 
+     long PNR;
     String food;
     boolean water;
     String snacks;
 
 
-    public Regular_Ticket(String food, String snacks,boolean water) {
-        super(46245,"Chd","MP",6622,5,"9:am","11:00am",5000,true,new Flight("AIR INDIA",6262,"Confirmed",80),new Address("Gupta colony","Kalka","Haryana",133302),new Contact("Paras",97292254,"parasramola972@gmail.com"));
+    public Regular_Ticket(String food, String snacks, boolean water,long PNR) {
+        super("Regular ticket", 547923, "CHANDIGARH", "CANADA", 7988412, 4, "4:00 AM", "10:00 PM", 35000, true);
+        this.PNR=PNR;
         this.food=food;
-        this.snacks=snacks;
-        this.water= water;
-
-    }
-       public void printticketdetails(){
-           System.out.println( "ticket number: " + Ticketnumber + " Source: " + source + " destination: " + destination +  " booking number: " + bookingnumber + " Numberseatsbooked: " + numberseatsbooked + " Time_Departure: " + time_Departure + " time_Arrival: " + time_Arrival +  " Priceofticket: " + priceofticket + " TicketConfirmed: " + ticketConfirmed );
-
-       }
-
-
-
-
-    public void printPNR() {
-
-    }
-
-    public void getFlight() {
-        System.out.println();
-
-    }
-
-
-
-    public String  getFood() {
-        System.out.println(food);
-        return food;
-    }
-
-
-    public boolean isWater() {
-        System.out.println(isWater());
-        return water;
-    }
-
-    public String getSnacks() {
-        System.out.println(snacks);
-        return snacks;
-    }
-
-
-    public void setFood(String food) {
-        this.food = food;
-    }
-
-    public void setWater(boolean water) {
-        this.water = water;
-    }
-
-    public void setSnacks(String snacks) {
         this.snacks = snacks;
+        this.water = water;
+
     }
+    public void printPNR() {
+        System.out.println("Printing PNR from regular ticket:" + " " + PNR);
+
+    }
+
 }
