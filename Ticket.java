afcfb2d539ab87a957a2b4d64d228dd05f7e8d74
balparas321 <parasramola97299@gmail.com>
2022-02-02 00:41:43 +0530

    updated Ticket class

--- Ticket.java
+++ Ticket.java
@@ -1,37 +1,32 @@
 package Reservation_System;
 
 public abstract class Ticket  {
-
+      String Ticket_name;
      long  Ticketnumber;
      String source;
      String destination;
      long bookingnumber;
      int numberseatsbooked;
-     int PNR;
      String time_Departure;
      String time_Arrival;
      int priceofticket;
      boolean ticketConfirmed;
-     Flight flight;
-     Address address;
-     Contact contact;
 
 
 
-    public
-    Ticket(long Ticketnumber, String source, String destination, long bookingnumber, int numberseatsbooked,  String time_Departure, String time_Arrival, int priceofticket, boolean ticketConfirmed, Flight flight,Address address,Contact contact) {
-       this.Ticketnumber = Ticketnumber;
+
+
+  public  Ticket( String Ticket_name ,long Ticketnumber, String source, String destination, long bookingnumber, int numberseatsbooked, String time_Departure, String time_Arrival, int priceofticket, boolean ticketConfirmed) {
+       this.Ticket_name=Ticket_name;
+        this.Ticketnumber = Ticketnumber;
         this.source = source;
         this.destination = destination;
         this.bookingnumber = bookingnumber;
         this.numberseatsbooked = numberseatsbooked;
-        this.PNR = PNR;
         this.time_Departure = time_Departure;
         this.time_Arrival = time_Arrival;
         this.priceofticket = priceofticket;
         this.ticketConfirmed = ticketConfirmed;
-        this.address=address;
-        this.contact=contact;
 
 
 
